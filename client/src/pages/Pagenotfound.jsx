1b22a5b89c2d47fd5c9d60ad0bdcddcb40c2bb2d
sai teja <saiteja93926@gmail.com>
2024-12-29 14:57:48 +0530

    project added

---
+++ client/src/pages/Pagenotfound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+function Pagenotfound() {
+  return (
+    <Layout>
+      <h1>Pagenot found</h1>
+    </Layout>
+  )
+}
+
+export default Pagenotfound
